2572c95550191407cea1f5355c4a90f4824a591e
ayeshasaifniazi <58334229+ayeshasaifniazi@users.noreply.github.com>
2019-11-29 20:39:03 +0500

    Add files via upload

---
+++ Assingment 2.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # OPERANDS
+
+# In[3]:
+
+
+original_sum = 30
+number_to_be_added = 5
+new_sum = original_sum + 3 + number_to_be_added 
+
+print(new_sum)
+
+
+# In[4]:
+
+
+sum = 5+5
+print(sum)
+
+
+# In[5]:
+
+
+sub=30-60
+print(sub)
+
+
+# In[6]:
+
+
+ply = 4*5
+print(ply)
+
+
+# In[7]:
+
+
+div =50/10
+print(div)
+
+
+# In[8]:
+
+
+print (3+3)
+
+
+# In[9]:
+
+
+loss = 10-20
+print(loss)
+
+
+# In[10]:
+
+
+dozen = 12*2 
+print(dozen)
+
+
+# In[13]:
+
+
+pops=15/3
+print (pops)
+
+
+# In[14]:
+
+
+num= .083
+total = num+110
+print(total)
+
+
+# In[15]:
+
+
+num=20
+another_num=3.5
+sum_of_nums= num + another_num
+print(sum_of_nums)
+
+
+# In[16]:
+
+
+left_over=10%3
+print(left_over)
+
+
+# In[17]:
+
+
+left_over=9%3
+print(left_over)
+
+
+# In[18]:
+
+
+age=55
+age = age+1
+age +=2
+print(age)
+
+
+# In[19]:
+
+
+age=55
+age+=12
+print(age)
+
+
+# In[20]:
+
+
+age=55
+age-=2
+print(age)
+
+
+# In[21]:
+
+
+age=20
+age*=2
+print(age)
+
+
+# In[22]:
+
+
+cost=1+3*5
+print(cost)
+
+
+# In[23]:
+
+
+result=(2*4)*(4+7)
+print(result)
+
+
+# # # string Method 
+
+# In[24]:
+
+
+a="hello world"
+b=a.capitalize()
+print(b)
+
+
+# In[25]:
+
+
+a= "hello World"
+b=a.center(30)
+print (b)
+
+
+# In[28]:
+
+
+a= "hello world hello hello"
+b=a.count("hello")
+print(b)
+
+
+# In[29]:
+
+
+a= "hello world"
+b= a.find("world")
+print(b)
+
+
+# In[30]:
+
+
+a="h\te\tl\tl\to"
+b=a.expandtabs(2)
+print(b)
+
+
+# In[31]:
+
+
+a= "hello world"
+b= a.upper()
+print(b)
+
+
+# In[32]:
+
+
+a="hello world"
+b= a.title()
+print(b)
+
+
+# In[33]:
+
+
+a="hello world"
+b= a.startswith("hi")
+print(b)
+
+
+# In[34]:
+
+
+a= "hello world"
+b= a.startswith("hello")
+print (b)
+
+
+# In[35]:
+
+
+a="hello\nworld"
+b=a.splitlines()
+print(b)
+
+
+# In[36]:
+
+
+a="hello world"
+b= a.strip()
+print (b)
+
+
+# In[37]:
+
+
+a="hello world"
+b= a.lower()
+print(b)
+
+
+# In[38]:
+
+
+a= "hello world"
+b= a.swapcase()
+print(b)
+
+
+# In[39]:
+
+
+a="HEllo WORld"
+b= a.swapcase()
+print(b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[40]:
+
+
+a=("hello", "world")
+b= '#'.join(a)
+print(b)
+
